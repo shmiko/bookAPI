e46b22e508d0351350d0e921efdb4a2e13374a0a
shmiko <shmiko@gmail.com>
2019-02-03 17:13:21 +1100

    post

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const bookRouter = express.Router();
+const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+
+    console.log(book);
+    res.json(book);
+  })
+  .get((req, res) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  });
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    });
+  });
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Nodemon API!');
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
