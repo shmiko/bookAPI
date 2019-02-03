6882b2c6e4213a01c39ca96739f7a49d26a464c5
shmiko <shmiko@gmail.com>
2019-02-03 17:45:03 +1100

    add seperate routes using book model tested

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,37 +1,37 @@
 const express = require('express');
 
-function rooutes(Book){
-const bookRouter = express.Router();
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
+function routes(Book) {
+  const bookRouter = express.Router();
+  bookRouter.route('/books')
+    .post((req, res) => {
+      const book = new Book(req.body);
 
-    // console.log(book);
-    book.save();
-    res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
+      book.save();
+      return res.status(201).json(book);
+    })
+    .get((req, res) => {
+      const query = {};
+      if (req.query.genre) {
+        query.genre = req.query.genre;
       }
-      return res.json(books);
+      Book.find(query, (err, books) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(books);
+      });
     });
-  });
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+      Book.findById(req.params.bookId, (err, book) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
     });
-  });
+
   return bookRouter;
-};
+}
 
-module.export = routes;
+module.exports = routes;
