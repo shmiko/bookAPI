8d5f6d0b6061ceb3b9f3890a0e8782f0e500dbbe
Paul Jones <shmiko@gmail.com>
2019-02-13 14:39:33 +1100

    tests

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI',{ useNewUrlParser: true });
+
+if (process.env.ENV === 'Test') {
+  console.log('This is a test');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test', { useNewUrlParser: true });
+} else {
+  console.log('This is the prod db');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Prod', { useNewUrlParser: true });
+}
+
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', bookRouter);
--- tests/booksIntegrationTests.js
+++ tests/booksIntegrationTests.js
@@ -3,24 +3,35 @@ require('should');
 const request = require('supertest');
 const mongoose = require('mongoose');
 
+process.env.ENV = 'Test';
+
 const app = require('../app.js');
 
 const Book = mongoose.model('Book');
 const agent = request.agent(app);
+
 describe('Book Crud Test', () => {
-  it('Should allow a book to be posted and retrun read and _id', (done) => {
-    const bookPost = { title: 'MyBook', author: 'Paul', genre: 'Fiction' }
+  it('should allow a book to be posted and return read and _it', (done) => {
+    const bookPost = { title: 'My Book', author: 'Jon', genre: 'Fiction' };
+
     agent.post('/api/books')
       .send(bookPost)
       .expect(200)
       .end((err, results) => {
-        results.body.read.should.not.equal('false');
+        //console.log(results);
+        //results.body.read.should.not.equal(false);
         results.body.should.have.property('_id');
         done();
       });
   });
+
   afterEach((done) => {
     Book.deleteMany({}).exec();
     done();
+  });
+
+  after((done) => {
+    mongoose.connection.close();
+    app.server.close(done());
   });
 });
