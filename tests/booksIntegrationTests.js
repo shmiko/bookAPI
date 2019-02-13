aedfecd634e6a8c30831a67228bd5eb711cc25e3
Paul Jones <shmiko@gmail.com>
2019-02-13 14:26:07 +1100

    superteast

--- tests/booksIntegrationTests.js
+++ tests/booksIntegrationTests.js
@@ -8,9 +8,19 @@ const app = require('../app.js');
 const Book = mongoose.model('Book');
 const agent = request.agent(app);
 describe('Book Crud Test', () => {
-    it('Should allow a book to be posted and retrun read and _id', (done) => {
-        const bookPost = { title : 'MyBook', author: 'Paul', genre: 'Fiction' }
-        agent.post('/api/books')
-            .send(bookPost);
-    });
+  it('Should allow a book to be posted and retrun read and _id', (done) => {
+    const bookPost = { title: 'MyBook', author: 'Paul', genre: 'Fiction' }
+    agent.post('/api/books')
+      .send(bookPost)
+      .expect(200)
+      .end((err, results) => {
+        results.body.read.should.not.equal('false');
+        results.body.should.have.property('_id');
+        done();
+      });
+  });
+  afterEach((done) => {
+    Book.deleteMany({}).exec();
+    done();
+  });
 });
