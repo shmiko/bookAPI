14b05dfc23a5fc4a1dea4aef0ab038d53377c500
Paul Jones <shmiko@gmail.com>
2019-02-13 13:51:12 +1100

    superteast done

--- tests/booksIntegrationTests.js
+++ tests/booksIntegrationTests.js
@@ -1,4 +1,16 @@
 require('should');
 
 const request = require('supertest');
+const mongoose = require('mongoose');
 
+const app = require('../app.js');
+
+const Book = mongoose.model('Book');
+const agent = request.agent(app);
+describe('Book Crud Test', () => {
+    it('Should allow a book to be posted and retrun read and _id', (done) => {
+        const bookPost = { title : 'MyBook', author: 'Paul', genre: 'Fiction' }
+        agent.post('/api/books')
+            .send(bookPost);
+    });
+});
